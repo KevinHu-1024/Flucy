1b2ba6a021db5116ceb1badf87ed41f92064debf
Kevin <hi-bai-du@163.com>
2016-04-26 22:37:19 +0800

    release

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 var title = "--- The fuck must be the ugliest page I have writen, but it works."
+var json = null;
+var str ='';
+var obj = {
+    challenges: []
+};
 function a() {
-    var json = JSON.parse(source.value);
-    var str ='';
-    var obj = {
-        challenges: []
-    };
+    json = JSON.parse(source.value);
     for(var i = 0; i < json.challenges.length; i++) {
         var cur = json.challenges[i];
         str += "<h4 style='color:green;text-decoration:underline'>" + (i+1) + "." + cur.title +"</h4>"
@@ -21,5 +22,21 @@ function a() {
     translate.innerHTML = "<h3 style='color:red'>" + json.name +"</h3>" + "<a href='https://github.com/KevinHu-1024' style='text-align:right;font-size:12px;color:grey;text-decoration:none'>" + title + "</a>" + str;
 }
 function b() {
-    
+    var collection = translate.getElementsByTagName("textarea");
+    var i = 0;
+    // console.log(collection);
+    for (var m = 0; m < obj.challenges.length; m++) {
+        var tar = obj.challenges[m];
+        for (var j = 0; j < tar.dis.length; j++) {
+            tar.dis[j] = collection[i].value;
+            i ++;
+        }
+    }
+    console.log(obj);
+    for (var k = 0; k < obj.challenges.length; k++) {
+        var cur = obj.challenges[k];
+        json.challenges[k].description = cur;
+    }
+    console.log(json);
+    source.value = JSON.stringify(json, null, 2);
 }
